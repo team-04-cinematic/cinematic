fae0b4f29f6e8afc4703500dcbe5bf54e2884088
amal <amal95salah@gmail.com>
2022-11-15 15:54:57 +0200

    move fetchMovies from scripts.js to home.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 import constructUrl from './utils/urls.js';
 import renderHome from './components/home.js';
 
-// This function is to fetch movies. You may need to add it or change some part in it in order
-// to apply some of the features.
-const fetchMovies = async () => {
-  const url = constructUrl('movie/now_playing');
-  const res = await fetch(url);
-  return res.json();
-};
+
 
 // Don't touch this function please
 const autorun = async () => {
-  const movies = await fetchMovies();
-  renderHome(movies.results);
+  renderHome();
 };
 
 document.addEventListener('DOMContentLoaded', autorun);
