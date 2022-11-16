74119def8ac5354931ec762d59616edfa76c104d
amal <amal95salah@gmail.com>
2022-11-17 01:20:52 +0200

    navbar

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import constructUrl from './utils/urls.js';
 import renderHome from './components/home.js';
+import renderMovie from './components/single-movie.js';
 
 // This function is to fetch movies. You may need to add it or change some part in it in order
 // to apply some of the features.
@@ -11,7 +12,17 @@ const fetchMovies = async () => {
 
 // Don't touch this function please
 const autorun = async () => {
+  const search = document.getElementById("search");
   const movies = await fetchMovies();
+  search.addEventListener(("keypress"), (event) => {
+    if (event.charCode === 13) { // key code of the keybord key
+      const result = movies.results.find(({ title }) => title.toUpperCase() === search.value.toUpperCase());
+      if (result != undefined) {
+        search.value = " "
+        renderMovie(result.id)
+      };
+    }
+  });
   renderHome(movies.results);
 };
 
